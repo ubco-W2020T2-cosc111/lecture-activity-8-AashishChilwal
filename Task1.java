6dac55f21fe5db8a134ebfe3cb735a33543b083d
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-15 17:16:23 -0600

    Finished Task1

--- Task1.java
+++ Task1.java
@@ -1,36 +1,38 @@
 import java.lang.Math.*;
 import java.util.Scanner;
-public class Task1
+public class Main
  {
 	public static void main(String[] args) {
+	    double a [] = new double [100]; 
 	    double total= 0,average;
-	    int above=0,below = 0,i,f=0; 
+	    int above=0,below = 0,i,f=0,b=0,a_1=0; 
 	    Scanner input = new Scanner(System.in);
 		for ( i = 0;i<100;i++)
 		{
-		    double a [] = new double [i]; 
 		    System.out.print("Enter a number (-1 to end): ");
 		    a[i] = input.nextInt();
 		    if (a [i] == -1)
 		        break;
 		     f++;
-		}
-		for (int j = 0;j<a.length;j++)
-		{
 		    total = total+a[i];
-		    if (below > a[i] ){
-		        below ++;}
-		    else if (above >= a[i]){
-		        above++;}
-		    else
-		    {
 		}
+			average = total/f;
+		for (i = 0;i<f;i++)
+		{
+		  if (a[i] >= average){
+		    a_1++;
+		  }
+		  else if (a[i] < average ){
+		    b++;
+		      
+		  }
+		    else
+		    {}
 		        
 		}
-		average = total/a.length;
 		System.out.println("Average: " + average);
-		System.out.println("Total number of scores: "+ a.length);
-		System.out.println("Number of scores above or equal to the average: " + above);
-		System.out.println("Number of scores below the average: " + below);
+		System.out.println("Total number of scores: "+ f);
+		System.out.println("Number of scores above or equal to the average: " + a_1);
+		System.out.println("Number of scores below the average: " + b);
 	 }
  }
